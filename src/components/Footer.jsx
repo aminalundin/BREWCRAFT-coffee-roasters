fc99785c108619b4f05cb1ddff027a0519ac3bc2
aminalundin <lundinamina1@gmail.com>
2025-01-07 21:50:21 +0100

    styled content for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,25 +6,25 @@ function Footer() {
         <>
             <div className="footer-container">
                 <div className="get-in-touch">
-                    <p>GET IN TOUCH</p>
+                    <p className="footer-subheading">GET IN TOUCH</p>
                     <p>Contact</p>
                     <p>Careers</p>
                 </div>
                 <div className="socials">
-                    <p>SOCIALS</p>
+                    <p className="footer-subheading">SOCIALS</p>
                     <p>Instagram</p>
                     <p>Facebook</p>
                     <p>X</p>
                 </div>
                 <div className="resources">
-                    <p>RESOURCES</p>
+                    <p className="footer-subheading">RESOURCES</p>
                     <p>Delivery / Shipping</p>
                     <p>Refund Policy</p>
                     <p>Privacy Policy</p>
                 </div>
                 <div className="subscribe">
-                    <p>SUBSCRIBE</p>
-                    <input type="text" className="sub" />
+                    <p className="footer-subheading">SUBSCRIBE</p>
+                    <input type="text" className="sub" placeholder="Email address" />
                     <p>Sign up to get exclusive pre-release access to new product
                         launches and Limited Edition coffees.</p>
                 </div>
