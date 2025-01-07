95fbb42519e9e2843c1337861de717c27b72db20
aminalundin <lundinamina1@gmail.com>
2025-01-07 21:41:09 +0100

    added content to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,36 @@ function Footer() {
 
     return (
         <>
-        <div className="footer-container">
-            <div className="get-in-touch"></div>
-            <div className="socials"></div>
-            <div className="resources"></div>
-            <div className="subscribe"></div>
-        </div>
-        <div className="copyright-container">
+            <div className="footer-container">
+                <div className="get-in-touch">
+                    <p>GET IN TOUCH</p>
+                    <p>Contact</p>
+                    <p>Careers</p>
+                </div>
+                <div className="socials">
+                    <p>SOCIALS</p>
+                    <p>Instagram</p>
+                    <p>Facebook</p>
+                    <p>X</p>
+                </div>
+                <div className="resources">
+                    <p>RESOURCES</p>
+                    <p>Delivery / Shipping</p>
+                    <p>Refund Policy</p>
+                    <p>Privacy Policy</p>
+                </div>
+                <div className="subscribe">
+                    <p>SUBSCRIBE</p>
+                    <input type="text" className="sub" />
+                    <p>Sign up to get exclusive pre-release access to new product
+                        launches and Limited Edition coffees.</p>
+                </div>
+            </div>
+            <div className="copyright-container">
 
-        </div>
+            </div>
         </>
     )
-} 
+}
 
 export default Footer;
