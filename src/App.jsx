4e0d1d1ef7754343f8ddedfd63496f7475f218f7
aminalundin <lundinamina1@gmail.com>
2025-01-07 20:05:37 +0100

    added info text and shop button

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,26 @@ function App() {
 
   return (
     <>
-    <div className="container">
+      <div className="container">
 
-      <main>
-    <div className="logo"><img src={logo} alt="logo-type" /></div>
-      </main>
+        <main>
+          <div className="logo"><img src={logo} alt="logo-type" /></div>
+        </main>
 
-      <aside>
+        <aside>
+          <div className="info-text">
+            <p>At BrewCraft, we believe coffee is more than a morning ritual—it’s an art form.
+              As an independent coffee roaster, we carefully source the finest beans from around the globe,
+              celebrating the unique stories and flavors each region has to offer. Every batch is roasted with precision and passion,
+              ensuring every cup is a journey of bold taste and exceptional quality.
+              Whether you’re a seasoned coffee enthusiast or just starting your craft coffee adventure,
+              BrewCraft is here to awaken your senses, one perfect brew at a time. Welcome to the art of coffee.</p>
+          </div>
 
-      </aside>
+          <div className="shop-button">
+            <p>SHOP OUR BEANS</p>
+          </div>
+        </aside>
 
       </div>
     </>
