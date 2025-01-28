606863f35bd78680fcd73d6baf8b96db9dcf0c7a
aminalundin <lundinamina1@gmail.com>
2025-01-28 16:33:40 +0100

    added product photos, styled first product

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,30 +1,32 @@
 import React from "react";
-import prodOne from '../images/signature-stamp-cinnam.png'
+import prodOne from '../images/signature.png'
 function Shop() {
 
     return (
         <>
             <div className="shop-container">
-                    <div className="roast-header">
-                        <p>SIGNATURE ROAST</p>
-                    </div>
                 <div className="roast-1">
                     <div className="prod-image-one">
                         <img src={prodOne} alt="signature-roast-product-image" />
                     </div>
+
                     <div className="roast-info-section">
+                        <div className="roast-header">
+                            <h1>SIGNATURE ROAST</h1>
                         <div className="roast-info">
                             <p>Discover the warmth and depth of our Signature Blend, a harmonious roast with rich,
                                 full-bodied flavors and a delicate hint of cinnamon spice.
                                 Perfectly balanced, this blend is crafted to bring a cozy, aromatic touch to your everyday coffee ritual.</p>
                         </div>
+                        </div>
                         <div className="add-to-cart">
                             <p>ADD TO CART</p>
                         </div>
                     </div>
-
-
+                    
                 </div>
+
+                <div className="roast-2"></div>
             </div>
         </>
     )
