f841bee0eb118eafde8780571fca42686464a7d5
aminalundin <lundinamina1@gmail.com>
2025-03-04 16:49:19 +0100

    added mini logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import miniLogo from '../images/mini-logo.png'
 
 function Header( {setActiveSection} ) {
 
@@ -7,9 +8,10 @@ function Header( {setActiveSection} ) {
 
        
         <div className="header">
-            <p onClick={() => setActiveSection("home")} className="nav-link">HOME</p>
             <p onClick={() => setActiveSection("shop")} className="nav-link">SHOP</p>
             <p onClick={() => setActiveSection("about")} className="nav-link">ABOUT</p>
+            <p onClick={() => setActiveSection("home")} className="nav-link">HOME</p>
+            <img src={miniLogo} alt="logo" />
         </div>
        
         </>
